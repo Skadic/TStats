91535795eac90de178f4e2098d66a9b3e575b9ca
Skadic <etieti22@gmail.com>
2022-07-19 01:56:14 +0200

    Remove React from dependencies

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -10,5 +8,5 @@ export default defineConfig({
     config: {
       applyBaseStyles: true
     }
-  }), react(), svelte()]
+  }), svelte()]
 });
