fde40262c75195c1c0f3ef2fd21c16ab20867036
Etienne Palanga <e.palanga@disroot.org>
2022-08-23 19:00:37 +0200

    Add an .env file to facilitate secrets

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,13 @@
-use rocket::{response::content::RawJson, tokio::sync::Mutex};
-use rosu_v2::{OsuBuilder, Osu};
-use sqlx::{sqlite::SqlitePoolOptions, Sqlite, Pool};
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::{Request, Response};
-use rocket::http::Status;
 use rocket::http::Header;
+use rocket::http::Status;
+use rocket::{response::content::RawJson, tokio::sync::Mutex};
+use rocket::{Request, Response};
+use rosu_v2::{Osu, OsuBuilder};
+use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod tournament;
 
@@ -28,45 +29,56 @@ impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
             name: "Attaching CORS headers to responses",
-            kind: Kind::Response
+            kind: Kind::Response,
         }
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
         println!("Attached Cors Headers to response");
     }
 }
 
-
 #[launch]
 async fn rocket() -> _ {
+    dotenv::dotenv().ok();
 
-    let client_id = std::env::var("MPSTAT_CLIENT_ID")
-        .expect("MPSTAT_CLIENT_ID not set")
+    let client_id = std::env::var("OSU_CLIENT_ID")
+        .expect("OSU_CLIENT_ID not set")
         .parse::<u64>()
-        .expect("MPSTAT_CLIENT_ID must be an unsigned integer");
-    let client_secret = std::env::var("MPSTAT_CLIENT_SECRET")
-        .expect("MPSTAT_CLIENT_SECRET not set");
+        .expect("OSU_CLIENT_ID must be an unsigned integer");
+    let client_secret = std::env::var("OSU_CLIENT_SECRET").expect("OSU_CLIENT_SECRET not set");
 
     let pool = SqlitePoolOptions::new()
         .max_connections(4)
-        .connect("test.db").await
+        .connect("test.db")
+        .await
         .expect("Error connecting to database");
 
     rocket::build()
         .attach(CORS)
         .mount("/api", routes![hello, cors_fix])
-        .mount("/api/tournament", routes![tournament::create_tournament, tournament::get_by_shorthand, tournament::get_all])
+        .mount(
+            "/api/tournament",
+            routes![
+                tournament::create_tournament,
+                tournament::get_by_shorthand,
+                tournament::get_all
+            ],
+        )
         .manage::<Mutex<Osu>>(Mutex::new(
             OsuBuilder::new()
                 .client_id(client_id)
                 .client_secret(client_secret)
-                .build().await
-                .expect("Could error connecting to osu api")
+                .build()
+                .await
+                .expect("Could error connecting to osu api"),
         ))
         .manage::<Pool<Sqlite>>(pool)
 }
--- backend/src/tournament.rs
+++ backend/src/tournament.rs
@@ -1,7 +1,6 @@
-
-use rocket::{serde::json::Json, State, http::Status, futures::TryStreamExt};
-use serde::{Serialize, Deserialize};
-use sqlx::{Pool, Sqlite, FromRow};
+use rocket::{http::Status, serde::json::Json, State};
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, Pool, Sqlite};
 
 #[derive(Serialize, Deserialize, FromRow, Debug)]
 pub struct Tournament {
@@ -10,9 +9,11 @@ pub struct Tournament {
     full_name: String,
 }
 
-
 #[post("/create", format = "application/json", data = "<tournament>")]
-pub async fn create_tournament(tournament: Json<Tournament>, sqlite_pool: &State<Pool<Sqlite>>) -> Status {
+pub async fn create_tournament(
+    tournament: Json<Tournament>,
+    sqlite_pool: &State<Pool<Sqlite>>,
+) -> Status {
     let tournament = tournament.into_inner();
 
     let query_result = sqlx::query("INSERT INTO Tournament(shorthand, full_name) VALUES ($1, $2)")
@@ -24,32 +25,34 @@ pub async fn create_tournament(tournament: Json<Tournament>, sqlite_pool: &State
 
     match query_result {
         Ok(_) => Status::Ok,
-        Err(_) => Status::InternalServerError
+        Err(_) => Status::InternalServerError,
     }
 }
 
-#[get("/by_shorthand/<tournament_shorthand>")] 
-pub async fn get_by_shorthand(tournament_shorthand: String, sqlite_pool: &State<Pool<Sqlite>>) -> (Status, Option<Json<Vec<Tournament>>>) {
+#[get("/by_shorthand/<tournament_shorthand>")]
+pub async fn get_by_shorthand(
+    tournament_shorthand: String,
+    sqlite_pool: &State<Pool<Sqlite>>,
+) -> (Status, Option<Json<Vec<Tournament>>>) {
     println!("Running shorthand endpoint: {tournament_shorthand}");
-    let result = sqlx::query_as::<_, Tournament>(
-        "SELECT * FROM Tournament WHERE shorthand = $1")
+    let result = sqlx::query_as::<_, Tournament>("SELECT * FROM Tournament WHERE shorthand = $1")
         .bind(tournament_shorthand)
-        .fetch_all(&**sqlite_pool).await;
+        .fetch_all(&**sqlite_pool)
+        .await;
     println!("Result: {:?}", &result);
 
     match result {
         Ok(vec) => (Status::Ok, Some(Json(vec))),
-        Err(_) => (Status::InternalServerError, None)
+        Err(_) => (Status::InternalServerError, None),
     }
 }
 
-#[get("/all")] 
+#[get("/all")]
 pub async fn get_all(sqlite_pool: &State<Pool<Sqlite>>) -> (Status, Option<Json<Vec<Tournament>>>) {
-    let result = 
-        sqlx::query_as::<_, Tournament>("SELECT * FROM Tournament")
-            .fetch_all(&**sqlite_pool);
+    let result =
+        sqlx::query_as::<_, Tournament>("SELECT * FROM Tournament").fetch_all(&**sqlite_pool);
     match result.await {
         Ok(vec) => (Status::Ok, Some(Json(vec))),
-        Err(_) => (Status::InternalServerError, None)
+        Err(_) => (Status::InternalServerError, None),
     }
 }
