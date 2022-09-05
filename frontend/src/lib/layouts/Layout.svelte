29969353f3c88bb9e2dbe7974d2eb3b9004b5428
Etienne Palanga <e.palanga@disroot.org>
2022-09-05 15:47:35 +0200

    Migrate to SvelteKit

---
+++ frontend/src/lib/layouts/Layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	let main: HTMLElement;
+
+	export let header = false;
+	export let headerHeight = 64;
+
+	export let footer = false;
+	export let footerHeight = 64;
+
+	console.log('Yes this is doing something');
+
+	$: if (main) {
+		main.style.setProperty('--header-height', headerHeight + 'px');
+		main.style.setProperty('--footer-height', footerHeight + 'px');
+	}
+</script>
+
+<main bind:this={main} class:header class:footer>
+	<slot />
+</main>
+
+<div class="slotHeader">
+	<slot name="header" />
+</div>
+
+<div class="slotFooter">
+	<slot name="footer" />
+</div>
+
+<style>
+	main {
+		@apply h-full w-full bg-bglight;
+	}
+
+	main.header {
+		padding-top: var(--header-height, 0px);
+	}
+
+	main.footer {
+		padding-bottom: var(--footer-height, 0px);
+	}
+
+	.slotHeader {
+		@apply fixed top-0 left-0 w-full;
+	}
+
+	.slotFooter {
+		@apply fixed bottom-0 left-0 w-full;
+	}
+</style>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { postcss } from "./postcss.config.cjs";
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  css: {
-    postcss
-  }
-})
+const config: UserConfig = {
+	plugins: [sveltekit()]
+};
+
+export default config;
