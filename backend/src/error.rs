b0862d5eb94c5750dfc1cc31650b5e70ebc5d699
Etienne Palanga <e.palanga@disroot.org>
2022-09-08 02:28:24 +0200

    Add some Docs and introduce a macro for errors

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -3,6 +3,18 @@ use std::fmt::Display;
 use redis::RedisError;
 use rosu_v2::prelude::OsuError;
 
+/// Implements simple from conversions for Error enum types
+macro_rules! quick_from_err {
+    ($err:path, $src:path, $var:ident) => {
+        impl From<$src> for $err {
+            fn from(err: $src) -> Self {
+                Self::$var(err)
+            }
+        }
+    }
+}
+
+/// An error that might occur when trying to retrieve a cached value using [crate::util::get_cached]
 #[derive(Debug)]
 pub enum CacheError {
     Redis(RedisError),
@@ -10,23 +22,9 @@ pub enum CacheError {
     Osu(OsuError),
 }
 
-impl From<RedisError> for CacheError {
-    fn from(err: RedisError) -> Self {
-        Self::Redis(err)
-    }
-}
-
-impl From<serde_json::error::Error> for CacheError {
-    fn from(err: serde_json::error::Error) -> Self {
-        Self::Json(err)
-    }
-}
-
-impl From<OsuError> for CacheError {
-    fn from(err: OsuError) -> Self {
-        Self::Osu(err)
-    }
-}
+quick_from_err!(CacheError, RedisError, Redis);
+quick_from_err!(CacheError, serde_json::error::Error, Json);
+quick_from_err!(CacheError, OsuError, Osu);
 
 impl Display for CacheError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
--- backend/src/util.rs
+++ backend/src/util.rs
@@ -7,6 +7,17 @@ use crate::error::CacheError;
 
 use redis::AsyncCommands;
 
+/// This attempts to find a value with the given key in the Redis cache.
+/// If it is not found, the supplier function is called to produce the value.
+/// That value cached under the given key as json and then returned.
+///
+/// # Errors
+/// 
+/// This function will return:
+/// A Redis variant of CacheError, if the communication or caching with Redis fails.
+/// A Json variant of CacheError, if serialization or deserialization fails.
+/// A different variant of CacheError depending on the errors returned by the supplier.
+/// 
 pub async fn get_cached<Out, Err, Supplier, Fut>(
     connection: &mut redis::aio::Connection,
     key: &str,
@@ -18,15 +29,18 @@ where
     Out: Serialize + DeserializeOwned,
     CacheError: From<Err>,
 {
+    // Try to find the value in the cache
     let val = if let Ok(cached) = connection.get::<_, String>(key).await {
+        // If found, deserialize and return it
         serde_json::from_str(&cached)?
     } else {
-        let calculated = supplier().await?;
-        let json_str = serde_json::to_string(&calculated)?;
+        // If not found, get the value from the supplier, serialize and cache it.
+        let supplied = supplier().await?;
+        let json_str = serde_json::to_string(&supplied)?;
         connection
             .set::<&str, String, String>(key, json_str)
             .await?;
-        calculated
+        supplied
     };
 
     Ok(val)
