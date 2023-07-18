45efb2b7875980fed70e209dc5ca1976499f5326
Etienne Palanga <e.palanga@disroot.org>
2023-07-19 01:13:49 +0200

    add docs to stage

--- backend/src/model/stage.rs
+++ backend/src/model/stage.rs
@@ -3,10 +3,14 @@ use surrealdb::opt::RecordId;
 
 use super::TableType;
 
+/// A stage in a tournament.
 #[derive(Debug, PartialEq, Serialize, Deserialize, Hash)]
 pub struct Stage {
+    /// The order of the stage in the tournament. The first stage is 0, the second is 1, etc.
     pub order: usize,
+    /// The stage's short name. For example, "QF", "RO16", etc.
     pub name: String,
+    /// The tournament this stage is in.
     pub tournament: RecordId
 }
 
